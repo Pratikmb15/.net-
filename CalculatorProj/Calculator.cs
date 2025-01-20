21362432be5e2776be7846a9f8906a7ba0be6ddd
Pratikmb15 <pratikbagul1508@gmail.com>
2025-01-20 15:50:31 +0530

    Add files via upload

---
+++ CalculatorProj/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProj
+{
+    public class Calculator
+    {
+        public static double Sum(double a, double b) {
+            double result = a + b;
+            return result;
+        }
+        public static double Diff(double a, double b)
+        {
+            double result2 = a - b;
+            return result2;
+        }
+    }
+}
